9a4de6980f9c9793ae7a303741d1224507486249
Денис Шагимарданов <denisshagimardanov@MacBook-Pro-Denis.local>
2024-04-19 21:16:30 +0500

    feat: add drag and drop

--- src/App.jsx
+++ src/App.jsx
@@ -1,141 +1,126 @@
+import React, {useState} from 'react';
 import "./App.css";
-import { Component, useState, useEffect } from "react";
-import PropTypes from "prop-types";
 
-const Card = ({ id, title, message, tag, user, onDelete ,changeCartText }) => {
-  const [inputValue, setInputValue] = useState(title);
+const App = () => {
+  const [boards, setBoards] = useState([
+    {id: 1, title: 'To Do', items: [{id: 1, title: "Пойти в магазин"}, {id: 2, title: "Выкинуть мусор"},  {id: 3, title: "Покушать"}]},
+    {id: 2, title: "In progress",  items: [{id: 4, title: 'Код ревью'}, {id: 5, title: "Задача на факториал"},  {id: 6, title: "Задача на фибоначчи"}]},
+    {id: 3, title: "Review", items: [{id: 7, title: "Снять видно"}, {id: 8, title: "Смонтировать"}, {id: 9, title: "Отрендерить"}]},
+    {id: 4, title: "Done", items: [{id: 7, title: "Снять видно"}, {id: 8, title: "Смонтировать"}, {id: 9, title: "Отрендерить"}]},
+  ])
 
-  const setValue = (e) => {
-    setInputValue(e.target.value);
-    changeCartText(e.target.value ,id)
-  };
+  const [newTaskTitle, setNewTaskTitle] = useState("");
 
-  return (
-    <div className="card">
-      <textarea
-        className="card__title"
-        value={inputValue}
-        onChange={setValue}
-      />
-      <div className="card__info">
-        <div className="avatar">{user}</div>
-        <div>{tag}</div>
-        <button className="delete" onClick={() => onDelete(id)}>
-          delete
-        </button>
-      </div>
-    </div>
-  );
-};
-
-class Wrapper extends Component {
-  constructor(props) {
-    super(props);
-  }
-  state = {
-    cardList: [],
-  };
-  componentDidMount() {
-    const cards = JSON.parse(localStorage.getItem("to-do")) || [];    
-    this.setState({
-      cardList: cards,
-    });
-  }
-  componentDidUpdate(){
-    localStorage.setItem("to-do", JSON.stringify(this.state.cardList));
-  }
-
-  createHandler = () => {
-    const newCard = {
-      id: Math.ceil(Math.random()*100),
-      title: "...",
-      message: "",
-      tag: "go",
-      user: "A",
-    };
-    this.setState({
-      cardList: [...this.state.cardList, newCard],
-    });
-  };
-
-  onDeleteCard = (id) => {
-    const newCardList = this.state.cardList.filter( item => item.id != id );
-
-    console.log(id)
-    this.setState({
-      cardList: newCardList,
-    });
-  };
-  changeCartText =(text, id)=>{
-
-
-    const newCardList= this.state.cardList.map(item=>{
-      if(item.id === id){
-        return {
-          id: item.id,
-          title: text,
-          message: item.message,
-          tag: item.tag,
-          user: item.user,
+  function addCardToFirstColumn() {
+    if (newTaskTitle.trim() !== "") {
+      const newCard = { id: Date.now(), title: newTaskTitle };
+      const updatedBoards = boards.map(board => {
+        if (board.id === 1) {
+          return { ...board, items: [...board.items, newCard] };
         }
+        return board;
+      });
+      setBoards(updatedBoards);
+      setNewTaskTitle(""); // Очищаем поле ввода после добавления задачи
+    }
+  }
+
+  const [currentBoard, setCurrentBoard] = useState(null)
+  const [currentItem, setCurrentItem] = useState(null)
+
+
+  function dragOverHandler(e) {
+    e.preventDefault()
+    if(e.target.className == 'item'){
+      e.target.style.boxShadow = '0 2px 3px gray'
+    }
+  }
+
+  function dragLeaveHandler(e) {
+    e.target.style.boxShadow = 'none'
+  }
+
+  function dragStartHandler(e, board, item) {
+  setCurrentBoard(board)
+    setCurrentItem(item)
+  }
+
+  function dragEndHandler(e) {
+    e.target.style.boxShadow = 'none'
+  }
+
+  function dropHandler(e, board, item) {
+    e.preventDefault()
+    const currentIndex = currentBoard.items.indexOf(currentItem)
+    currentBoard.items.splice(currentIndex, 1)
+    const dropIndex = board.items.indexOf(item)
+    board.items.splice(dropIndex + 1, 0, currentItem )
+    setBoards(boards.map(b => {
+      if (b.id === board.id) {
+        return board
       }
-      return item
-    })
-
-
-    this.setState({
-      cardList: newCardList,
-    });
+      if (b.id === currentBoard.id) {
+        return currentBoard
+      }
+      return b
+    }))
+    e.target.style.boxShadow = 'none'
   }
 
-  render() {
-    const { cardList } = this.state;
-    const { type, title } = this.props;
-    const classNames = [type, "colum-wrapper"];
-    return (
-      <div className={classNames.join(" ")}>
-        <h2 className="colum-wrapper__title">{title}</h2>
-        {cardList.map((card, index) => (
-          <Card
-            title={card.title}
-            message={card.message}
-            tag={card.tag}
-            user={card.user}
-            key={card.id}
-            onDelete={this.onDeleteCard}
-            changeCartText={this.changeCartText}
-            id={card.id}
-          />
-        ))}
-        <button className="create-card" onClick={this.createHandler}>
-          Create +
-        </button>
-      </div>
-    );
+  function dropCardHandler(e, board) {
+    board.items.push(currentItem)
+    const currentIndex = currentBoard.items.indexOf(currentItem)
+    currentBoard.items.splice(currentIndex, 1)
+    setBoards(boards.map(b => {
+      if (b.id === board.id) {
+        return board
+      }
+      if (b.id === currentBoard.id) {
+        return currentBoard
+      }
+      return b
+    }))
+    e.target.style.boxShadow = 'none'
   }
-}
 
-PropTypes.shape({
-  title: PropTypes.string,
-  message: PropTypes.string,
-  tag: PropTypes.string,
-  user: PropTypes.string,
-});
-
-Wrapper.propTypes = {
-  type: PropTypes.oneOf(["to-do", "in-progress", "reviev", "done"]).isRequired,
-};
-
-function App() {
-  
   return (
-    <div className="app">
-      <h1 className="title">Trello</h1>
-      <div className="desck">
-        <Wrapper title={"To-do"} type="to-do" />
+      <div className="app">
+
+        <div className="add-task">
+          <input
+              className="input-value"
+              type="text"
+              placeholder="Введите название задачи"
+              value={newTaskTitle}
+              onChange={(e) => setNewTaskTitle(e.target.value)}
+          />
+          <button className='btn' onClick={addCardToFirstColumn}>Add</button>
+        </div>
+
+        {boards.map(board =>
+            <div
+                className={`board board-${board.id}`}
+                onDragOver={(e) => dragOverHandler(e)}
+                onDrop={(e) => dropCardHandler(e, board)}
+            >
+              <div className="board__title">{board.title}</div>
+              {board.items.map(item =>
+                  <div
+                      onDragOver={(e) => dragOverHandler(e)}
+                      onDragLeave={e => dragLeaveHandler(e)}
+                      onDragStart={(e) => dragStartHandler(e, board, item)}
+                      onDragEnd={(e) => dragEndHandler(e)}
+                      onDrop={(e) => dropHandler(e, board, item)}
+                      className="todo"
+                      draggable={true}
+                      className="item"
+                  >
+                    {item.title}</div>
+              )}
+            </div>
+        )}
       </div>
-    </div>
   );
-}
+};
 
 export default App;
